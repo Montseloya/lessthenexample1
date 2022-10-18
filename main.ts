c63a0d38b450d6ea7979a2d07d991442f90416bc
Montseloya <113448172+Montseloya@users.noreply.github.com>
2022-10-18 08:47:23 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    info.changeScoreBy(1)
+})
+let projectile: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . . . 
+    . . . c 4 d 4 4 4 4 4 1 c . c c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+    . f 4 4 4 4 1 c 4 f 4 d f f f f 
+    . . f f 4 d 4 4 f f 4 c f c . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 0, 50)
+mySprite.x = 8
+mySprite.setStayInScreen(true)
+info.setScore(0)
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . f c c c c f . . . . . 
+        . . c c f b b 3 3 b b f c c . . 
+        . c b 3 3 b b c c b b 3 3 b c . 
+        . f 3 c c c b c c b c c c 3 f . 
+        f c b b c c b c c b c c b b c f 
+        c 3 c c b c c c c c c b c c 3 c 
+        c 3 c c c c c c c c c c c c 3 c 
+        . f b b c c c c c c c c b b f . 
+        . . f b b c 8 9 9 8 c b b f . . 
+        . . c c c f 9 3 1 9 f c c c . . 
+        . c 3 f f f 9 3 3 9 f f f 3 c . 
+        c 3 f f f f 8 9 9 8 f f f f 3 c 
+        f 3 c c f f f f f f f f c c 3 f 
+        f b 3 c b b f b b f b b c 3 b f 
+        . c b b 3 3 b 3 3 b 3 3 b b c . 
+        . . f f f f f f f f f f f f . . 
+        `, -150, 0)
+    projectile.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
+    if (info.score() < 10) {
+        projectile.vx += 100
+    }
+})
